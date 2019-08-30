547887f9f3f22eea7f58f7bf5cbfe835f6125a4e
Takeshi Tsukamoto <itometeam@gmail.com>
2019-08-30 13:38:26 +0900

    Add command line parser

--- src/main/kotlin/team/itome/accessibilityanalyzer/App.kt
+++ src/main/kotlin/team/itome/accessibilityanalyzer/App.kt
@@ -3,6 +3,9 @@
  */
 package team.itome.accessibilityanalyzer
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckPreset
 import com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResult.AccessibilityCheckResultType
 import com.google.android.apps.common.testing.accessibility.framework.AccessibilityHierarchyCheck
@@ -13,43 +16,54 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
 
-private const val OUTPUT_DIR =
-  "/Users/s04407/OSS/android-sunflower/app/build/outputs/apk/debug/crawl_output/app_firebase_test_lab"
+fun main(args: Array<String>) = AccessibilityCheckCommand().main(args)
 
-fun main(args: Array<String>) {
+class AccessibilityCheckCommand : CliktCommand() {
+  private val targetDir by option(
+    "--target",
+    help = "Target directory that contains accessibility.meta files"
+  ).required()
 
-  val files = File(OUTPUT_DIR).listFiles()
-    ?.filter { Regex("accessibility[0-9]+.meta").matches(it.name) }
-    ?: throw FileNotFoundException("No test target file found in $OUTPUT_DIR")
+  private val targetDirFullPath: String
+    get() = targetDir.replace("~", System.getProperty("user.home"))
 
-  for (file in files) {
-    val proto = file.inputStream().use { stream -> AccessibilityHierarchyProto.parseFrom(stream) }
-    val hierarchy = AccessibilityHierarchy.newBuilder(proto).build()
-    val results = runAccessibilityChecks(hierarchy)
+  override fun run() {
+    val files = File(targetDirFullPath).listFiles()
+      ?.filter { Regex("accessibility[0-9]+.meta").matches(it.name) }
+      ?: throw FileNotFoundException("No test target file found in $targetDirFullPath")
 
-    results
-      .filter {
-        it.type == AccessibilityCheckResultType.ERROR ||
-            it.type == AccessibilityCheckResultType.WARNING
-      }
-      .forEach {
-        @Suppress("UNCHECKED_CAST")
-        val checkClass = AccessibilityCheckPreset.getHierarchyCheckForClass(it.sourceCheckClass as Class<out AccessibilityHierarchyCheck>)
-        println(checkClass.getMessageForResult(Locale.JAPANESE, it))
-      }
-//      .map { it.toProto() }
+    for (file in files) {
+      val proto = file.inputStream().use { stream -> AccessibilityHierarchyProto.parseFrom(stream) }
+      val hierarchy = AccessibilityHierarchy.newBuilder(proto).build()
+      val results = runAccessibilityChecks(hierarchy)
+
+      results
+        .filter {
+          it.type == AccessibilityCheckResultType.ERROR ||
+              it.type == AccessibilityCheckResultType.WARNING
+        }
+        .forEach { result ->
+          @Suppress("UNCHECKED_CAST")
+          val checkClass = AccessibilityCheckPreset.getHierarchyCheckForClass(
+            result.sourceCheckClass as Class<out AccessibilityHierarchyCheck>
+          )
+          println(checkClass.getTitleMessage(Locale.JAPAN))
+          println(result.element)
+          println(checkClass.getMessageForResult(Locale.JAPAN, result))
+        }
 //      .forEachIndexed { index, outputProto ->
 //        val file = File(dir, "${inputFile.nameWithoutExtension}_result$index.meta")
 //        file.createNewFile()
 //        file.outputStream().use { outputProto.writeTo(it) }
 //      }
+    }
   }
-}
 
-private fun runAccessibilityChecks(
-  hierarchy: AccessibilityHierarchy
-): List<AccessibilityHierarchyCheckResult> {
-  return AccessibilityCheckPreset
-    .getAccessibilityHierarchyChecksForPreset(AccessibilityCheckPreset.LATEST)
-    .flatMap { it.runCheckOnHierarchy(hierarchy) }
+  private fun runAccessibilityChecks(
+    hierarchy: AccessibilityHierarchy
+  ): List<AccessibilityHierarchyCheckResult> {
+    return AccessibilityCheckPreset
+      .getAccessibilityHierarchyChecksForPreset(AccessibilityCheckPreset.LATEST)
+      .flatMap { it.runCheckOnHierarchy(hierarchy) }
+  }
 }
